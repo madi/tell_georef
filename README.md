ee57cb86ae3ebd0fa593349fdfc2d4047025dcb4
madi <dileomargherita@gmail.com>
2017-08-03 15:31:17 +0200

    read the metadata of a series of .jp2 files and create a .csv file with the names of the files that are georeferenced and another .csv file with the non-georeferenced.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Purpose of tell_georef.py
+
+The purpose of this script is to read the metadata of a series of .jp2 files and
+create a .csv file with the names of the files that are georeferenced and another
+.csv file with the non-georeferenced.
+It uses ExifTool http://owl.phy.queensu.ca/~phil/exiftool/ so you need to install
+it before running the script
---
+++ tell_georef.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+__author__ = "Margherita Di Leo"
+__date__   = "Thu Aug 3 2017"
+
+# The purpose of this script is to read the metadata of a series of .jp2 files and
+# create a .csv file with the names of the files that are georeferenced and another
+# .csv file with the non-georeferenced.
+# It uses ExifTool http://owl.phy.queensu.ca/~phil/exiftool/ so you need to install
+# it before running the script
+
+import os
+import subprocess
+import re
+
+# directory in which the files are:
+INPUT_FOLDER = "/run/media/madi/TOSHIBA EXT/PUGLIA/Orto_2015_5k"
+# directory where to write csv files
+OUTPUT_FOLDER = "/home/madi/Projects/parse_metadata_exiftool"
+
+# String to look for in the metadata:
+# Projected CS Type               : WGS84 UTM zone 33N
+CRS = "WGS84 UTM zone 33N"
+
+def isgeoreferenced(filename, metadata, CRS, georeferenced, nongeoreferenced):
+    '''Reads the result of exiftool and decide whether the file is georeferenced
+    or not.
+    '''
+    match = re.search(CRS, metadata)
+    if match:
+        georeferenced.append(filename)
+    else:
+        nongeoreferenced.append(filename)
+
+    return georeferenced, nongeoreferenced
+
+def writecsv(folder, lista, filename):
+
+    f = open(folder + os.sep + filename,'w')
+
+    for item in lista:
+        f.write(item + '\n')
+
+    f.close()
+
+
+
+if __name__ == '__main__':
+    # open the folder and creates a list of the .jp2 files
+    os.chdir(INPUT_FOLDER)
+    lista = os.listdir(INPUT_FOLDER)
+
+    # initialize lists of georeferenced and non-georeferenced files
+    georeferenced = []
+    nongeoreferenced = []
+
+    for tile in enumerate(lista):
+        filename = tile[1]
+        cmd = "exiftool " + filename
+        proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, shell = True)
+        (metadata, err) = proc.communicate()
+        # print "program output:", metadata
+        georeferenced, nongeoreferenced = isgeoreferenced(filename, \
+                                                      metadata, \
+                                                      CRS, \
+                                                      georeferenced, \
+                                                      nongeoreferenced)
+
+    # write output on csv files
+    writecsv(OUTPUT_FOLDER, georeferenced, "georef.csv")
+    writecsv(OUTPUT_FOLDER, nongeoreferenced, "non_georef.csv")
+
+    print "done"
+
+
+
+
+
+
+
+
+#
